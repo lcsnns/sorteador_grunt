a7b25573cba0d394f918421b0d6d52c95562f501
lucas nunes <145053610+lcsnns@users.noreply.github.com>
2024-01-22 17:18:23 -0300

    commit inicial

---
+++ src/styles/scripts/main.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentloaded', function() {
+    document.getElementById('form-sorteador').addEventListener('submit', function(evento) {
+        evento.preventDefault();
+        let numeroMaximo = document.getElementById('numero-maximo').value;
+        numeroMaximo = parseInt(numeroMaximo);
+
+        let numeroAleatorio = math.random() * numeroMaximo;
+        numeroAleatorio = math.round(numeroAleatorio + 1);
+
+
+        document.getElementById('resultado-valor').innerText = numeroAleatorio;
+    })
+})
